8a1442fe5f6dd73392dc46c467387bd8345b21c4
Yue Liu <augustayu717@gmail.com>
2014-12-25 20:29:45 +0800

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include<vector>
+#include<string>
+#include "Decision_Tree.h"
+using namespace std;
+int main() {
+	int i;
+	ofstream accurcise;
+	accurcise.open("test.txt");
+	for(i= 20; i<=400; i=i+5) {
+	Node* Decsion_tree = new Node;
+    ini(Decsion_tree, i);
+	create_tree(Decsion_tree);
+	no_cut_thumb(Decsion_tree);
+	accurcise << i << " " << test_accuracy(Decsion_tree) << endl;
+	}
+}
